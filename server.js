88db07bba800cf982b1fd4bf41cc5eed0901fbdb
Georgia Hunt <georgialhunt89@gmail.com>
2021-05-19 15:33:00 +0930

    Added data parsing for express

--- server.js
+++ server.js
@@ -6,7 +6,9 @@ const app = express();
 // Setting initial port
 const PORT = process.env.PORT || 2222;
 
-
+// Allow data parsing for Express
+app.use(express.urlencoded({ extended: true}));
+app.use(express.json());
 
 
 // Added Listener for PORT
