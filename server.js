4f04022d73b0b9dd8262d2c8bbc72d8ce83171e6
Georgia Hunt <georgialhunt89@gmail.com>
2021-05-19 15:46:59 +0930

    Added required htmlRoutes and apiRoutes

--- server.js
+++ server.js
@@ -10,6 +10,10 @@ const PORT = process.env.PORT || 2222;
 app.use(express.urlencoded({ extended: true}));
 app.use(express.json());
 
+// ROUTES for HTML and API's
+require('./routes/htmlRoutes')(app);
+require('./routes/apiRoutes')(app);
+
 
 // Added Listener for PORT
 app.listen(PORT, () => {
