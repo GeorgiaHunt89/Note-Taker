8d80d8f5695e6e04b3ed990f7e18ad157b353931
Georgia Hunt <georgialhunt89@gmail.com>
2021-05-21 00:57:21 +0930

    updated route and port

--- server.js
+++ server.js
@@ -4,13 +4,14 @@ const express = require('express');
 const app = express();
 
 // Setting initial port
-const PORT = process.env.PORT || 2222;
+const PORT = process.env.PORT || 3000;
 
 // Allow data parsing for Express
 app.use(express.urlencoded({ extended: true}));
 app.use(express.json());
+app.use(express.static(__dirname));
 
-// ROUTES for HTML and API's
+// ROUTES for HTML and API
 require('./routes/htmlRoutes')(app);
 require('./routes/apiRoutes')(app);
 
