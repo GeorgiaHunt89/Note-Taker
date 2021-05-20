9699114d23058a3ceac9f3e67e749c7296730d0c
Georgia Hunt <georgialhunt89@gmail.com>
2021-05-21 01:09:11 +0930

    Updated HTML Routes

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -5,11 +5,11 @@ const app = require('express');
 module.exports = (app) => {
 
 // GET route to notes.html
-app.get('./assets/public/notes.html', (req, res) => {res.sendFile(path.join(__dirname, '../public/notes.html'));
+app.get('/notes', (req, res) => {res.sendFile(path.join(__dirname, '../public/notes.html'));
 });
 
 // Get route index.html
-app.get('./assets/public/index.html', (req, res) => {res.sendFile(path.join(__dirname, '../public/index.html'));
+app.get('/', (req, res) => {res.sendFile(path.join(__dirname, '../public/index.html'));
 });
 
 // Default to homepage route
