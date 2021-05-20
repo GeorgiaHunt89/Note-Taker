0e300a1784f8b0f6431f880d41b8843d061299f7
Georgia Hunt <georgialhunt89@gmail.com>
2021-05-20 17:09:55 +0930

    GET, POST & DELETE requests added to apiRoute

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,5 +1,27 @@
 // Dependencies to hold and save data
 
-const apiRouter = require ('express').Router();
-
 const storeData = require('..db/storeData');
+
+module.exports = (app) => {
+
+// GET request
+app.get('/api/notes', (req, res) => res.json(storeData));
+
+// POST request
+app.post('api/notes', (req, res) => {
+    storeData
+    .addNote(req.body)
+    .then((noteData) => res.json(noteData))
+    .catch(err => res.status(400).send('The note is not correctly formatted and cannot be saved'));
+    });
+
+
+// DELETE request
+app.delete('api/notes/:id', (req, res) => {
+    storeData
+    .deleteData(req.params.id)
+    .then(() => res.json({ok: true}))
+    .catch(err => res.status(400).send('There is an error deleting this note'));
+});
+
+};
