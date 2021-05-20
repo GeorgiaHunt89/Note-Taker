dc709d7c0c9277f80ae369b124b02aab25aa40a8
Georgia Hunt <georgialhunt89@gmail.com>
2021-05-21 00:58:38 +0930

    Added read file and database write file functions

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,27 +1,54 @@
-// Dependencies to hold and save data
-const storeData = require('..db/storeData');
+// Dependencies
+const path = require('path');
+const fs = require('fs');
 const app = require('express');
+
 
 module.exports = (app) => {
 
+// Read File
+const storeData = data => {
+fs.readFile('./db/db.json', 'utf8', (err, data) => {
+    if (err) throw err;
+    console.log(err);
+
+// Updated Database
+const updateDatabase = data => {
+    fs.writeFile('db/db.json', JSON.stringify(storeData, '\t'), err => {
+        if (err){
+            console.log (err);
+            return;
+        } else {
+           console.log('Successfully updated your notes.') 
+        }
+    });
+}
+
 // GET request
-app.get('/api/notes', (req, res) => {res.json(storeData)});
+app.get('/api/notes', (req, res) => {res.json(storeData)
+});
 
 // POST request
-app.post('api/notes', (req, res) => {
-    storeData
-    .addNote(req.body)
-    .then((noteData) => res.json(noteData))
-    .catch(err => res.status(400).send('The note is not correctly formatted and cannot be saved'));
-    });
+app.post('/api/notes', (req, res) => {
+    let addNote = req.body;
+    storeData.push(addNote);
+    updateDatabase();
+    return console.log('Successfully added note: '`${addNote.title}`);
+});
+
+// GET request for specific id
+app.get('/api/notes/:id', (req, res) => {
+    res.json(storeData[req.params.id]);
+});
 
 
 // DELETE request
-app.delete('api/notes/:id', (req, res) => {
-    storeData
-    .deleteData(req.params.id)
-    .then(() => res.json({ok: true}))
-    .catch(err => res.status(400).send('There is an error deleting this note'));
+app.delete('/api/notes:id', (req, res) => {
+    storeData.spice(req.params.id, 1);
+    updateDatabase();
+    console.log('Successfully deleted note '`${req.params.id}`);
 });
 
+});
 };
+}
