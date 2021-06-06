fe88cff6265bdb23607826fb9240b6bf8a5d7804
Georgia Hunt <georgialhunt89@gmail.com>
2021-06-06 14:09:12 +0930

    updated path and added Json path

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,19 +1,20 @@
 // Dependencies
 const path = require('path');
+const fs = require('fs');
 const app = require('express');
 
 module.exports = (app) => {
 
-// GET route to notes.html
-app.get('/notes', (req, res) => {res.sendFile(path.join(__dirname, '../public/notes.html'));
-});
+ // Get route index.html
+app.get('/', (req, res) => res.sendFile(path.join(__dirname, '../public/index.html')));   
 
-// Get route index.html
-app.get('/', (req, res) => {res.sendFile(path.join(__dirname, '../public/index.html'));
-});
+// GET route to notes.html
+app.get('/notes', (req, res) => res.sendFile(path.join(__dirname, '../public/notes.html')));
 
 // Default to homepage route
-app.get('*', (req, res) => {res.sendFile(path.join(__dirname, '../public/index.html'));
-});
+app.get('*', (req, res) => res.sendFile(path.join(__dirname, '../public/index.html')));
+
+// Get route for Json saved notes
+app.get('/api/notes', (req, res) => res.sendFile(path.join(__dirname, './db/db.json')));
 
 };
